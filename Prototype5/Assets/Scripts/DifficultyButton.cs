03ac39713f296b1a6d62f724f0338e70984eaea9
AUDREYCAMARENA <142622382+AUDREYCAMARENA@users.noreply.github.com>
2024-04-11 13:43:36 -0700

    removed unused libraries. add serialidfield.

--- Prototype5/Assets/Scripts/DifficultyButton.cs
+++ Prototype5/Assets/Scripts/DifficultyButton.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
 public class DifficultyButton : MonoBehaviour
@@ -7,6 +6,7 @@ public class DifficultyButton : MonoBehaviour
     private Button button;
     private GameManager gameManager;
     public int difficulty;
+    [SerializedField] private int difficulty;
     // Start is called before the first frame update
     void Start()
     {
